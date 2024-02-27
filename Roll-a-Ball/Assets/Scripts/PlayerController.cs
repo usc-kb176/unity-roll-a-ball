5fc5b61729731bbdddfe622818e78899e949e339
usc-kb176 <k_b176@student.usc.edu.au>
2024-02-27 23:14:10 +1000

    Moving the Player
    Finished the tutorial for moving the player via unity learn roll-a-ball

---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+
+}
